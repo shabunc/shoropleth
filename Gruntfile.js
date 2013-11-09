aacd707be029e796a961ceccd02eb44cd938de46
Shagen Ogandzhanyan <shabunc@yandex-team.ru>
2013-11-09 23:45:58 +0400

    connect task introduced

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,32 +9,44 @@ module.exports = function(grunt) {
         return path.join(destBase, destPath.replace(/\.jade$/, '.html'));
       }
     });
- };
+  };
 
   grunt.initConfig({
-        compass: {
-            dist: {
-                options: {
-                    sassDir: 'src/scss',
-                    cssDir: 'build/css'
-                    }
-                }
-        },
-        jade: {
-          visual: {
-            options: {
-              data:{}
-            },
-            files: getJadeVisualFiles()
-          }
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          base: 'build/tests/visual',
+          keepalive: true
         }
-    });
+      }
+    },
+    compass: {
+        dist: {
+            options: {
+                sassDir: 'src/scss',
+                cssDir: 'build/css'
+                }
+            }
+    },
+    jade: {
+      visual: {
+        options: {
+          data:{}
+        },
+        files: getJadeVisualFiles()
+      }
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('visual', ['jade:visual']);
 
-    grunt.registerTask('default', ['compass:dist', 'visual']);
+  grunt.registerTask('visual', ['jade:visual']);
+
+  grunt.registerTask('default', ['compass:dist', 'visual']);
 
 }
