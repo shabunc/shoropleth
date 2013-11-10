01ede08d286833344b68d2c2835dcbea26be338d
Shagen Ogandzhanyan <shabunc@yandex-team.ru>
2013-11-11 03:39:13 +0400

    Resolves #4 - Color Palettes

--- tests/visual/js/modules/app.js
+++ tests/visual/js/modules/app.js
@@ -1,8 +1,9 @@
 define([
   'shoropleth',
+  'lib/shoropleth/palette/Palette',
 
   'd3'
-], function(Shoropleth) {
+], function(Shoropleth, Palette) {
 
   var RUS = {
     //.rotate([-105, 0]) .center([-10, 65]) .parallels([52, 64]) .scale(700) .translate([width / 2, height / 2]);
@@ -58,8 +59,15 @@ define([
 
 
   var drawMap = function(container_selector, data_url, params) {
-    var sh = new Shoropleth().init(container_selector, params);
+    params.resolvePolygonCSS = function(self) {
+      var palette = Palette.Palette9.Oranges;
+      var rgba = palette[Math.floor(Math.random() * palette.length)];
+      var color = ['rgb(',rgba.join(','),')'].join('');
+      console.log(color);
+      self.style.fill = color;
+    }
 
+    var sh = new Shoropleth().init(container_selector, params);
     sh.load(data_url);
   }
 
@@ -67,5 +75,6 @@ define([
   drawMap('#map-cis', 'data/cis.json', CIS);
   drawMap('#map-fo', 'data/rus-fo.json', RUS);
   drawMap('#map-rus', 'data/rus.json', RUS);
+  drawMap('#map-rus-rayons', 'data/rus-rayons.json', RUS);
   window.Shoropleth = Shoropleth;
 });
