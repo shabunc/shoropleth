0bcdd331deee3994f8cd508b931b0ce21d66c733
Shagen Ogandzhanyan <shabunc@yandex-team.ru>
2013-11-10 15:50:12 +0400

    getProjection as a separate method which can be overriden

--- src/js/modules/shoropleth.js
+++ src/js/modules/shoropleth.js
@@ -14,17 +14,15 @@ define([
     this.height = params.height;
     this.container = document.querySelector(container_selector);
 
-    console.log(this.container);
+    if (typeof this.params.getProjection === "function") {
+      this.getProjection = this.params.getProjection;
+    }
 
     this.control = d3.select(this.container).append('svg');
+    this.container.style.width = this.width + "px";
+    this.container.style.height = this.height + "px";
 
     return this;
-
-//    d3.json("uk.json", function(error, uk) {
-//      svg.append("path")
-//        .datum(topojson.feature(uk, uk.objects.subunits))
-//        .attr("d", d3.geo.path().projection(d3.geo.mercator()));
-//    });
   }
 
   Shoropleth.prototype.load = function(url) {
@@ -34,10 +32,30 @@ define([
     });
   }
 
+
+  Shoropleth.prototype.getProjection = function() {
+    var width = this.width;
+    var height = this.height;
+
+    return d3.geo.albers()
+      .center([0, 55.4])
+      .rotate([4.4, 0])
+      .parallels([50, 60])
+      .scale(600)
+      .translate([width / 2, height / 2]);
+  }
+
   Shoropleth.prototype.render = function(data) {
+    var subunits = topojson.feature(data, data.objects.subunits);
+
+    var projection = this.getProjection();
+    console.log("PROJE", projection);
+
+    var path = d3.geo.path().projection(projection);
+
     this.control.append("path")
-      .datum(topojson.feature(data, data.objects.subunits))
-      .attr("d", d3.geo.path().projection(d3.geo.mercator()));
+      .datum(subunits)
+      .attr("d", path);
   }
 
   Shoropleth.prototype.getSVGControl = function() {
--- tests/visual/js/modules/app.js
+++ tests/visual/js/modules/app.js
@@ -2,9 +2,9 @@ define([
   'shoropleth'
 ], function(Shoropleth) {
 
-  var sh = new Shoropleth().init('#uk', {
-    width: 960,
-    height: 1160
+  var sh = new Shoropleth().init('#uk1', {
+    width: 96,
+    height: 116
   });
 
   sh.load('data/uk.json');
