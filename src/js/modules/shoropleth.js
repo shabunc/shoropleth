77266e03f1a3ef34493b68b613bc261f9464c745
Shagen Ogandzhanyan <shabunc@yandex-team.ru>
2013-11-10 17:44:00 +0400

    changing properties of projection - each separately

--- src/js/modules/shoropleth.js
+++ src/js/modules/shoropleth.js
@@ -18,6 +18,26 @@ define([
       this.getProjection = this.params.getProjection;
     }
 
+    if (typeof this.params.center != "undefined") {
+      this.setCenter(this.params.center);
+    }
+
+    if (typeof this.params.rotate != "undefined") {
+      this.setRotate(this.params.rotate);
+    }
+
+    if (typeof this.params.scale != "undefined") {
+      this.setScale(this.params.scale);
+    }
+
+    if (typeof this.params.translate != "undefined") {
+      this.setTranslate(this.params.translate);
+    }
+
+    if (typeof this.params.parallels != "undefined") {
+      this.setParallels(this.params.parallels);
+    }
+
     this.control = d3.select(this.container).append('svg');
     this.container.style.width = this.width + "px";
     this.container.style.height = this.height + "px";
@@ -38,18 +58,57 @@ define([
     var height = this.height;
 
     return d3.geo.albers()
-      .center([0, 55.4])
-      .rotate([4.4, 0])
-      .parallels([50, 60])
-      .scale(600)
+      .center(this.getCenter())
+      .rotate(this.getRotate())
+      .parallels(this.getParallels())
+      .scale(this.getScale())
       .translate([width / 2, height / 2]);
+  }
+
+  Shoropleth.prototype.setCenter = function(center) {
+    this.center = center;
+    return this;
+  }
+  Shoropleth.prototype.getCenter = function() {
+    return this.center;
+  }
+
+  Shoropleth.prototype.setRotate = function(rotate) {
+    this.rotate = rotate;
+    return this;
+  }
+  Shoropleth.prototype.getRotate = function() {
+    return this.rotate;
+  }
+
+  Shoropleth.prototype.setScale = function(scale) {
+    this.scale = scale;
+    return this;
+  }
+  Shoropleth.prototype.getScale = function() {
+    return this.scale;
+  }
+
+  Shoropleth.prototype.setTranslate = function(translate) {
+    this.translate = translate;
+    return this;
+  }
+  Shoropleth.prototype.getTranslate = function() {
+    return this.translate;
+  }
+
+  Shoropleth.prototype.setParallels = function(parallels) {
+    this.parallels = parallels;
+    return this;
+  }
+  Shoropleth.prototype.getParallels = function() {
+    return this.parallels;
   }
 
   Shoropleth.prototype.render = function(data) {
     var subunits = topojson.feature(data, data.objects.subunits);
 
     var projection = this.getProjection();
-    console.log("PROJE", projection);
 
     var path = d3.geo.path().projection(projection);
 
--- tests/visual/js/modules/app.js
+++ tests/visual/js/modules/app.js
@@ -1,10 +1,28 @@
 define([
-  'shoropleth'
+  'shoropleth',
+
+  'd3'
 ], function(Shoropleth) {
 
   var sh = new Shoropleth().init('#uk1', {
     width: 96,
-    height: 116
+    height: 116,
+    center: [0, 55.4],
+    rotate: [4.4, 0],
+    parallels: [50, 60],
+    scale: 600
+  });
+
+  sh.load('data/uk.json');
+
+
+  var sh = new Shoropleth().init('#uk2', {
+    width: 96,
+    height: 116,
+    center: [0, 55.4],
+    rotate: [4.4, 0],
+    parallels: [50, 60],
+    scale: 600
   });
 
   sh.load('data/uk.json');
